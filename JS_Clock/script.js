25d0bcfa50b3dc82f1cdc4354809c064cc9b9a15
Bill <wkernan@gmail.com>
2016-12-11 12:16:29 -0600

    js clock

---
+++ JS_Clock/script.js
@@ -0,0 +1,26 @@
+const secHand = document.querySelector('.sec-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+	const now = new Date();
+	const seconds = now.getSeconds();
+	const secondsDegrees = ((seconds / 60) * 360) + 90;
+	secHand.style.transform = `rotate(${secondsDegrees}deg)`;
+	if(secondsDegrees == 90) {
+		secHand.className = 'hand-off sec-hand';
+	} else {
+		secHand.className = 'hand sec-hand';
+	}
+
+	const minutes = now.getMinutes();
+	const minutesDegrees = ((minutes / 60) * 360) + 90;
+	minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+	const hours = now.getHours();
+	const hoursDegrees = ((hours / 12) * 360) + 90;
+	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+}
+
+setInterval(setDate, 1000);
