915fcdc632c42ba1c59677dba720768b3a18ae6c
Bill <wkernan@gmail.com>
2017-01-01 16:22:47 -0600

    green screen function

--- Webcam_Fun/scripts.js
+++ Webcam_Fun/scripts.js
@@ -25,7 +25,8 @@ function paintToCanvas() {
 		ctx.drawImage(video, 0, 0, width, height);
 		let pixels = ctx.getImageData(0, 0, width, height);
 		// pixels = redEffect(pixels);
-		pixels = rgbSplit(pixels);
+		// pixels = rgbSplit(pixels);
+		pixels = greenScreen(pixels);
 		ctx.putImageData(pixels, 0, 0);
 	}, 16);
 }
@@ -57,6 +58,32 @@ function rgbSplit(pixels) {
 		pixels.data[i + 100] = pixels.data[i + 1]; //green
 		pixels.data[i -150] = pixels.data[i + 2]; //blue
 	}
+	return pixels;
+}
+
+function greenScreen(pixels) {
+	const levels = {};
+
+	document.querySelectorAll('.rgb input').forEach((input) => {
+		levels[input.name] = input.value;
+	});
+
+	for(i = 0; i < pixels.data.length; i += 4) {
+		red = pixels.data[i + 0];
+		green = pixels.data[i + 1];
+		blue = pixels.data[i + 2];
+		alpha = pixels.data[i + 3];
+
+		if(red >= levels.rmin
+			&& green >= levels.gmin
+			&& blue >= levels.bmin
+			&& red <= levels.rmax
+			&& green <= levels.gmax
+			&& blue <= levels.bmax) {
+			pixels.data[i + 3] = 0;
+		}
+	}
+
 	return pixels;
 }
 
